eec420ad14ab522137b5299bf9da3ec3e372f80f
Akshaybagai52 <akshaybagai52@gmail.com>
2025-02-17 23:47:39 +0530

    feat: added port

--- rust-api/server/src/main.rs
+++ rust-api/server/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8000");
+    let server = Server::new("127.0.0.1:8000".to_string());
     server.run();
 }
 
@@ -7,6 +7,15 @@ struct Server {
     addr: String,
 }
 
-impl Server [
-    
-]
+impl Server {
+
+    fn new(addr: String) -> Self {
+        Self {
+            addr: addr
+        }
+    }
+
+    fn run(self) {
+        println!("Listening on http://{}", self.addr);
+    }
+}
