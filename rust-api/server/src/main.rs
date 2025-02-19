0d751f99aa968005f68aaff440eeec368f187a35
Akshaybagai52 <akshaybagai52@gmail.com>
2025-02-19 22:57:05 +0530

    feat: added Option enum

--- rust-api/server/src/main.rs
+++ rust-api/server/src/main.rs
@@ -3,30 +3,34 @@ fn main() {
     let delete = Method::DELETE;
     let post = Method::POST;
     let put = Method::PUT;
-    let server = Server::new("127.0.0.1:8000".to_string());
+    let server = server::Server::new("127.0.0.1:8000".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-impl Server {
-
-    fn new(addr: String) -> Self {
-        Self {
-            addr: addr
+mode server {
+    pub struct Server {
+        addr: String,
+    }
+    
+    impl Server {
+    
+       pub fn new(addr: String) -> Self {
+            Self {
+                addr: addr
+            }
+        }
+    
+        pub fn run(self) {
+            println!("Listening on http://{}", self.addr);
         }
     }
-
-    fn run(self) {
-        println!("Listening on http://{}", self.addr);
-    }
 }
+
+
 
 struct Request {
     path: String,
-    query_string: String,
+    query_string: Option<String>,
     method: String,
 }
 
